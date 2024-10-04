63c1cee9a30132236622de3830b96b026a39f9ea
arthurwinck <apwinck@gmail.com>
2024-10-04 16:36:46 -0300

    adding to messaged

--- listeners/commands/feedback.py
+++ listeners/commands/feedback.py
@@ -53,12 +53,11 @@ def parse_prompt(prompt: str) -> dict:
 def add_parameters(parsed_prompt: dict) -> str:
   # Adicionar na respoista
   template_prompt = f"""
-
-    Considerando um profissional de um profissional de uma empresa de TI, me auxilie a dar um feedback para um colega a 
+    Considerando um profissional de uma empresa de TI, me auxilie a dar um feedback para um colega a 
     partir do seguinte comportamento que observei por parte dele (lembre-se que esse é o ponto fundamental desse texto): {parsed_prompt["comportamento"]}.
     Use o primeiro nome desse username: {parsed_prompt["usuario"]}
     para iniciar a mensagem de feedback. Leve em consideração que você irá criar um feedback para o seguinte perfil de pessoa:
-    {parsed_prompt["tipoPessoa"]}. Com base no perfil de pessoa, crie um feedback adequado. Construa o feedback seguindo esse tipo: {parsed_prompt["tipoFeedback"]}
+    {parsed_prompt["tipoPessoa"]}. Com base no perfil de pessoa, crie um feedback adequado. Construa o feedback seguindo esse modelo de feedback existente (SCI, CNV ou espontâneo): {parsed_prompt["tipoFeedback"]}
     """
   return template_prompt
 
--- listeners/events/app_messaged.py
+++ listeners/events/app_messaged.py
@@ -11,6 +11,32 @@ Handles the event when a direct message is sent to the bot, retrieves the conver
 and generates an AI response.
 """
 
+def parse_prompt(prompt: str) -> dict:
+    split_prompt = prompt.split(" ")
+
+    try:
+        user_to_receive = split_prompt[0]
+        person_type = split_prompt[1]
+        feedback_type = split_prompt[2]
+
+        # feedback_type = getTipoFeedbackTextByEnum()
+
+        behavior = split_prompt[3]
+    except Exception as e:
+        raise Exception("Mensagem mal formatada")
+
+    return {"usuario": user_to_receive, "tipoPessoa": person_type, "tipoFeedback": feedback_type, "comportamento": behavior}
+
+def add_parameters(parsed_prompt: dict) -> str:
+  # Adicionar na respoista
+  template_prompt = f"""
+    Considerando um profissional de uma empresa de TI, me auxilie a dar um feedback para um colega a 
+    partir do seguinte comportamento que observei por parte dele (lembre-se que esse é o ponto fundamental desse texto): {parsed_prompt["comportamento"]}.
+    Use o primeiro nome desse username: {parsed_prompt["usuario"]}
+    para iniciar a mensagem de feedback. Construa o feedback seguindo esse modelo de feedback existente (SCI, CNV ou espontâneo): {parsed_prompt["tipoFeedback"]}.
+    No final da mensagem adicione "Feedback para {parsed_prompt["usuario"]}"
+    """
+  return template_prompt
 
 def app_messaged_callback(client: WebClient, event: dict, logger: Logger, say: Say):
     channel_id = event.get("channel")
@@ -24,7 +50,9 @@ def app_messaged_callback(client: WebClient, event: dict, logger: Logger, say: S
 
             if thread_ts:  # Retrieves context to continue the conversation in a thread.
                 conversation = client.conversations_replies(channel=channel_id, limit=10, ts=thread_ts)["messages"]
-                conversation_context = parse_conversation(conversation[:-1])
+                parsed_prompt = conversation[:-1]
+                
+                conversation_context = parse_conversation(add_parameters(parsed_prompt))
 
             waiting_message = say(text=DEFAULT_LOADING_TEXT, thread_ts=thread_ts)
             response = get_provider_response(user_id, text, conversation_context, DM_SYSTEM_CONTENT)
