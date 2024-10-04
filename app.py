c601d090b56183244da0b01d70ea5e020ba768da
erikorsolin <depaulaerik670@gmail.com>
2024-10-04 16:22:57 -0300

    feat: add remind

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from remind import *
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -16,3 +17,5 @@ register_listeners(app)
 # Start Bolt app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+
+executar()
